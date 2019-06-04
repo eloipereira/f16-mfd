d00b34aadb39877d13b1bceb924d44f32a734597
Eloi Pereira <eloipereira@gmail.com>
2019-06-04 10:59:33 +0100

    add video menu

--- mfd.py
+++ mfd.py
@@ -4,6 +4,8 @@ from select import select
 from evdev import InputDevice, categorize, ecodes
 from PIL import Image, ImageTk
 import configparser as cp
+import imageio
+import re
 
 # Config
 config = cp.ConfigParser()
@@ -13,14 +15,15 @@ dev = InputDevice(config['DEFAULT']['MFD_DEVICE'])
 filename = config['DEFAULT']['STF_FILE']
 img_folder = config['DEFAULT']['IMAGES_FOLDER']
 img_extension = config['DEFAULT']['FIG_EXT']
+video_extension = config['DEFAULT']['VID_EXT']
 
-class MFD_Window(Frame):
-    def __init__(self, state_transition = None, master=Tk(), img_folder = img_folder, img_extension = img_extension):
+class MFD_App(Frame):
+    def __init__(self, state_transition = None, master=Tk(), img_folder = img_folder):
         Frame.__init__(self,master)
         self.state_transition = state_transition
         self.master = master
         self.init_window()
-        self.img_path = img_folder + "/{0!s}." + img_extension
+        self.img_path = img_folder + "/{0!s}" 
 
     def init_window(self):
         self.master.geometry("800x600")
@@ -28,20 +31,34 @@ class MFD_Window(Frame):
         self.pack(fill=BOTH, expand=1)
         
     def show_menu(self):
-        img = ImageTk.PhotoImage(Image.open(self.img_path.format(self.state_transition.state)))
-        self.panel = Label(self, image=img)
-        self.panel.image = img
-        self.panel.pack(fill = "both", expand = "yes")
-        self.update_idletasks()
-        self.update()
+        if re.match("(\S+)."+img_extension , self.state_transition.state.name): 
+            img = ImageTk.PhotoImage(Image.open(self.img_path.format(self.state_transition.state)))
+            self.panel = Label(self, image=img)
+            self.panel.image = img
+            self.panel.pack(fill = "both", expand = "yes")
+            self.update_idletasks()
+            self.update()
+        elif re.match("(\S+)."+video_extension , self.state_transition.state.name):
+            print("First menu cannot be a video.")
+        else:
+            print("Unknown extension.")
 
     def update_menu(self):
-        img2 = ImageTk.PhotoImage(Image.open(self.img_path.format(self.state_transition.state)))
-        self.panel.configure(image=img2)
-        self.panel.image = img2
-        self.update_idletasks()
-        self.update()
-
+        if re.match("(\S+)."+img_extension , self.state_transition.state.name): 
+            img2 = ImageTk.PhotoImage(Image.open(self.img_path.format(self.state_transition.state)))
+            self.panel.configure(image=img2)
+            self.panel.image = img2
+            self.update_idletasks()
+        elif re.match("(\S+)."+video_extension , self.state_transition.state.name):
+            video = imageio.get_reader(self.img_path.format(self.state_transition.state))
+            for frame in video.iter_data():
+                frame_img = ImageTk.PhotoImage(Image.fromarray(frame))
+                self.panel.configure(image=frame_img)
+                self.panel.image = frame_img
+                self.update_idletasks()
+        else:
+            print("Unknown extension.")
+                
     def events_loop(self):
         r, w, x = select([dev], [],[])
         for e in r[0].read():
@@ -50,8 +67,7 @@ class MFD_Window(Frame):
                     self.update_menu()
         self.after(100,self.events_loop())
 
-
 st = StateTransition(from_file = True, filename = filename)
-app = MFD_Window(st)
+app = MFD_App(st)
 app.show_menu()
 app.events_loop()
